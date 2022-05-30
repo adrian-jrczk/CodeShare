3b5925386ed875c6fb74b74d7343ba71c3c596df
Adrian Jurczak <103427657+adrian-jrczk@users.noreply.github.com>
2022-05-30 19:46:05 +0200

    Fixed missing User id generation

--- src/main/java/codeshare/user/User.java
+++ src/main/java/codeshare/user/User.java
@@ -18,6 +18,7 @@ import java.util.List;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @Column(unique = true)
     private String name;
